a07f2c5fee0b5b09fed6c9c48244c4dc91c0f950
Demiao Lin <ldmiao@gmail.com>
2012-06-13 22:02:53 +0800

    use localhost for test

--- test.py
+++ test.py
@@ -1,17 +1,16 @@
 import redis
 import time
 
-r = redis.StrictRedis(host='192.168.1.61', port=8323)
+r = redis.StrictRedis(host='localhost', port=8323)
 #time.sleep(1)
-print r.set("key", 'b'*50)
-print r.get("key")
+#print r.set("key", 'b'*50)
+#print r.get("key")
 
-"""
 for i in range(10000):
     key = 'foo_%d'%i
-    r.set(key, 'b'*i)
-    #if i%100==0:
-    print key, "->",r.get(key)
+    r.set(key, 'b'*(i%1000))
+    if i%100==0:
+        print key, "->",r.get(key)
 
 print r.incr('ddd')
 print r.get('ddd')
@@ -20,6 +19,5 @@ print r.get('ddd')
 print r.incr('ddd')
 print r.get('ddd')
 
-"""
 
 
