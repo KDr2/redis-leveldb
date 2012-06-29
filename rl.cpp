b59c61539a56df4c8d8f95691c00d08e952030b9
root <root@vm-debian64>
2012-06-29 09:50:04 +0800

    add includes

--- rl.cpp
+++ rl.cpp
@@ -6,6 +6,11 @@
  */
 
 #include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
 #include <sys/socket.h>
 
 #include "rl_util.h"
--- rl_request.cpp
+++ rl_request.cpp
@@ -6,6 +6,10 @@
  */
 
 #include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include <sys/socket.h>
 
 #include <gmp.h>
